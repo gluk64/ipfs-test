9e05e5c66ace28a47b4d0dfe2531baa462113bcb
Alex <gluk64@gmail.com>
2018-11-07 16:16:55 +0800

    ipfs storage working

--- src/test.js
+++ src/test.js
@@ -1,27 +1,42 @@
 'use strict'
 
 const IPFS = require('ipfs')
+const bs58 = require('bs58')
 
 const ipfs = new IPFS({
 //    repo: 'ipfs/pubsub-demo/' + Math.random(),
     config: {
         Addresses: {
             Swarm: [
-                '/dns4/ws-star.discovery.libp2p.io/tcp/443/wss/p2p-websocket-star'
+                //'/dns4/ws-star.discovery.libp2p.io/tcp/443/wss/p2p-websocket-star'
             ]
         }
     }
 })
 
+// API docs: https://github.com/ipfs/interface-ipfs-core#api
+
 ipfs.once(`ready`, async () =>  {
 
-    // add data
-    const filesAdded = await ipfs.files.add(Buffer.from('Hello World 101'))
-    const hash = filesAdded[0].hash
-    console.log('Added file:', hash)
+    // generate content
+    const content = {text: 'hello'}
 
-    // get data
+    // add to IPFS as file
+    const buffer = Buffer.from(JSON.stringify(content))
+    const filesAdded = await ipfs.files.add(buffer, {hash: 'sha3-256'})
+    const hash = filesAdded[0].hash // base 58
+    const hash16 = '0x' + bs58.decode(hash).toString('hex')
+    console.log(`Added file: ${hash} (${hash16})`)
+    console.log('filesAdded[0]', filesAdded[0])
+
+    // pin file
+    await ipfs.pin.add(hash)
+
+    // fetch file by hash
     const file = await ipfs.files.cat(hash)
-    console.log(file.toString('utf8'))
+    console.log('Loaded content:',  file.toString('utf8'))
 
+    // remove pin
+    await ipfs.pin.rm(hash)
+    console.log(`Pin removed for ${hash}.`)
 })
